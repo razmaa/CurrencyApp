977fba990b750f61b0216e72fdb6b06f522214c3
razmaa <145837511+razmaa@users.noreply.github.com>
2024-02-04 01:06:05 +0400

    Refactor
    Rewrote code to make it easy to understand

--- CurrnecyConverterApp/Scenes/CurrencyConverterPage/View/Components/CurrencyConverterView.swift
+++ CurrnecyConverterApp/Scenes/CurrencyConverterPage/View/Components/CurrencyConverterView.swift
@@ -8,120 +8,152 @@
 import SwiftUI
 
 struct CurrencyConverterView: View {
+    //MARK: - Properties
     @StateObject private var viewModel = CurrencyConverterViewModel()
     @State private var amount = ""
     @State private var baseCurrency = "USD"
     @State private var targetCurrency = "EUR"
-
+    
     var body: some View {
-         VStack {
-             Text("Currency Converter")
-                 .font(.system(size: 22))
-                 .foregroundColor(.white)
-                 .padding()
-             
-             TextField("Amount", text: $amount)
-                 .keyboardType(.numberPad)
-                 .padding()
-                 .background(Color.darkGray)
-                 .foregroundColor(.white)
-                 .cornerRadius(10)
-
-             DatePicker("Select a Date", selection: $viewModel.selectedDate, displayedComponents: .date)
-                 .padding()
-                 .background(Color.darkGray)
-                 .foregroundColor(.white)
-                 .cornerRadius(10)
-
-             HStack {
-                 Button(action: { viewModel.convert(amount: amount, from: baseCurrency, to: targetCurrency) }) {
-                     Text("Convert")
-                         .font(.headline)
-                         .foregroundColor(.white)
-                         .padding()
-                         .background(Color.primary)
-                         .cornerRadius(10)
-                 }
-                 .disabled(amount.isEmpty)
-                 
-                 Button(action: { viewModel.convertHistorical(amount: amount, from: baseCurrency, to: targetCurrency, date: viewModel.selectedDate) }) {
-                     Text("Convert on Selected Date")
-                         .font(.headline)
-                         .foregroundColor(.white)
-                         .padding()
-                         .background(Color.primary)
-                         .cornerRadius(10)
-                 }
-                 .disabled(amount.isEmpty)
-                 
-             }
-             .padding()
-             
-             HStack( spacing: 30) {
-                 Picker("Base Currency", selection: $baseCurrency) {
-                     ForEach(viewModel.currencies, id: \.self) { currency in
-                         Text(currency).tag(currency)
-                     }
-                 }
-                 .frame(minWidth: 100)
-                 .padding()
-                 .foregroundColor(Color.primary)
-                 .background(Color.darkGray)
-                 .cornerRadius(10)
-
-                 Button(action: { swapCurrencies() }) {
-                     Image(systemName: "rectangle.2.swap")
-                         .foregroundColor(.primary)
-                 }
-                 .disabled(amount.isEmpty)
-
-                 Picker("Target Currency", selection: $targetCurrency) {
-                     ForEach(viewModel.currencies, id: \.self) { currency in
-                         Text(currency).tag(currency)
-                     }
-                 }
-                 .frame(minWidth: 100)
-                 .padding()
-                 .background(Color.darkGray)
-                 .foregroundColor(.white)
-                 .cornerRadius(10)
-             }
-             .padding()
-             
-             Spacer()
-             
-             VStack {
-                 VStack {
-                     Text("Today")
-                         .font(.title3)
-                         .foregroundColor(.primary)
-                     
-                     Text("\(viewModel.conversionResult) \(targetCurrency)")
-                         .font(.title)
-                         .foregroundColor(.primary)
-                 }
-                 
-                 Divider()
-                 
-                 if (viewModel.historicalConversion != nil) {
-                     VStack {
-                         Text("Chosen date")
-                             .font(.title3)
-                             .foregroundColor(.primary)
-                         
-                         Text("\(viewModel.historicalConversion ?? "0.0") \(targetCurrency)")
-                             .font(.title)
-                             .foregroundColor(.primary)
-                     }
-                 }
-             }
-
-             Spacer()
-         }
-         .background(Color.background)
-     }
-
-    func swapCurrencies() {
+        NavigationView {
+            ZStack {
+                AppColorsSwiftUI.background.ignoresSafeArea(.all)
+                mainView
+            }
+        }
+    }
+    
+    //MARK: - Components
+    private var mainView: some View {
+        VStack() {
+            titleView
+            amountField
+            datePicker
+            conversionButtons
+            currencyPickers
+            Spacer()
+            conversionResults
+            Spacer()
+        }
+        .padding(.vertical, 5)
+    }
+    
+    private var titleView: some View {
+        Text("Currency Converter")
+            .font(.system(size: 22))
+            .foregroundColor(.white)
+            .padding()
+    }
+    
+    
+    private var amountField: some View {
+        TextField("Amount", text: $amount)
+            .keyboardType(.numberPad)
+            .padding()
+            .background(Color.darkGray)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+    
+    private var datePicker: some View {
+        DatePicker("Select a Date", selection: $viewModel.selectedDate, displayedComponents: .date)
+            .padding()
+            .background(Color.darkGray)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .onChange(of: viewModel.selectedDate) { oldValue, newValue in
+                if !amount.isEmpty {
+                    viewModel.convert(amount: amount, from: baseCurrency, to: targetCurrency)
+                    viewModel.convertHistorical(amount: amount, from: baseCurrency, to: targetCurrency, date: newValue)
+                }
+            }
+    }
+    
+    private var conversionButtons: some View {
+        HStack {
+            convertButton(action: {
+                viewModel.convert(amount: amount, from: baseCurrency, to: targetCurrency)
+                viewModel.convertHistorical(amount: amount, from: baseCurrency, to: targetCurrency, date: viewModel.selectedDate)
+            }, title: "Convert")
+        }
+        .padding()
+    }
+    
+    private func convertButton(action: @escaping () -> Void, title: String) -> some View {
+        Button(action: action) {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.primary)
+                .cornerRadius(10)
+        }
+        .disabled(amount.isEmpty)
+    }
+    
+    private var currencyPickers: some View {
+        HStack(spacing: 30) {
+            currencyPicker(selection: $baseCurrency)
+            swapButton
+            currencyPicker(selection: $targetCurrency)
+        }
+        .padding()
+    }
+    
+    private var swapButton: some View {
+        Button(action: { swapCurrencies() }) {
+            Image(systemName: "rectangle.2.swap")
+                .foregroundColor(.primary)
+        }
+        .disabled(amount.isEmpty)
+    }
+    
+    private var conversionResults: some View {
+        VStack {
+            conversionResult(title: "Today", result: viewModel.conversionResult)
+            Divider()
+            conversionResult(title: "Chosen date", result: viewModel.historicalConversion ?? "0.0")
+        }
+    }
+    
+    //MARK: - Methods
+    private func currencyPicker(selection: Binding<String>) -> some View {
+        Picker("Currency", selection: selection) {
+            ForEach(viewModel.currencies, id: \.self) { currency in
+                Text(currency).tag(currency)
+            }
+        }
+        .frame(minWidth: 100)
+        .padding()
+        .background(Color.darkGray)
+        .foregroundColor(.white)
+        .cornerRadius(10)
+        .onChange(of: baseCurrency) {
+            if !amount.isEmpty {
+                viewModel.convert(amount: amount, from: baseCurrency, to: targetCurrency)
+                viewModel.convertHistorical(amount: amount, from: baseCurrency, to: targetCurrency, date: viewModel.selectedDate)
+            }
+        }
+        .onChange(of: targetCurrency) {
+            if !amount.isEmpty {
+                viewModel.convert(amount: amount, from: baseCurrency, to: targetCurrency)
+                viewModel.convertHistorical(amount: amount, from: baseCurrency, to: targetCurrency, date: viewModel.selectedDate)
+            }
+        }
+    }
+    
+    private func conversionResult(title: String, result: String) -> some View {
+        VStack {
+            Text(title)
+                .font(.title3)
+                .foregroundColor(.primary)
+            Text("\(result) \(targetCurrency)")
+                .font(.title)
+                .foregroundColor(.primary)
+        }
+    }
+    
+    private func swapCurrencies() {
         let temp = baseCurrency
         baseCurrency = targetCurrency
         targetCurrency = temp
@@ -131,3 +163,4 @@ struct CurrencyConverterView: View {
         }
     }
 }
+
