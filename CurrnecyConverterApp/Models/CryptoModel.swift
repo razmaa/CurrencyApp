34c5a5debe71f59aab1680623814705a1b5463fd
razmaa <145837511+razmaa@users.noreply.github.com>
2024-02-07 03:30:49 +0400

    Create files for Crypto List

---
+++ CurrnecyConverterApp/Models/CryptoModel.swift
@@ -0,0 +1,8 @@
+//
+//  CryptoModel.swift
+//  CurrnecyConverterApp
+//
+//  Created by nika razmadze on 07.02.24.
+//
+
+import Foundation
---
+++ CurrnecyConverterApp/Scenes/CryptoListPage/View/CryptoListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CryptoListViewController.swift
+//  CurrnecyConverterApp
+//
+//  Created by nika razmadze on 07.02.24.
+//
+
+import UIKit
+
+class CryptoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
