6a2e923676ae94ecfe2554f7d302ed799acf6566
razmaa <145837511+razmaa@users.noreply.github.com>
2024-02-07 04:55:24 +0400

    Create crypto list view controller

--- CurrnecyConverterApp/Scenes/CryptoListPage/View/CryptoListViewController.swift
+++ CurrnecyConverterApp/Scenes/CryptoListPage/View/CryptoListViewController.swift
@@ -6,24 +6,96 @@
 //
 
 import UIKit
+import NetworkManager
 
 class CryptoListViewController: UIViewController {
+    //MARK: - Properties
+    private var tableView: UITableView!
+    private var searchController: UISearchController!
+    private var viewModel = CryptoListViewModel()
+    private var filteredCryptos: [Crypto] = []
 
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    //MARK: - Methods
+    private func setupUI () {
+        setupTableView()
+
+        viewModel.fetchCryptos { [weak self] in
+            DispatchQueue.main.async {
+                self?.filteredCryptos = self?.viewModel.cryptos ?? []
+                self?.tableView.reloadData()
+            }
+        }
+        setupSearchController()
+        navigationItem.title = "Crypto"
+    }
+    
+    private func setupTableView() {
+        tableView = UITableView(frame: view.bounds)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.allowsSelection = false
+        tableView.register(CryptoTableViewCell.self, forCellReuseIdentifier: "CryptoCell")
+        tableView.backgroundColor = UIColor.background
+        view.addSubview(tableView)
+    }
+    
+    private func setupSearchController() {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Crypto currencies"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
+        if let textField = searchController.searchBar.value(forKey: "searchField") as? UITextField {
+            textField.defaultTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        }
+
+    }
+
+}
+
+//MARK: - Extension(TableView)
+extension CryptoListViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredCryptos.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath) as! CryptoTableViewCell
+        let crypto = filteredCryptos[indexPath.row]
+        let image = viewModel.image(for: crypto)
+        cell.configure(with: crypto, image: image)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        80
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//MARK: - Extension(SearchController)
+extension CryptoListViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text, !searchText.isEmpty {
+            filteredCryptos = viewModel.cryptos.filter { crypto in
+                return crypto.name.lowercased().contains(searchText.lowercased())
+            }
+        } else {
+            filteredCryptos = viewModel.cryptos
+        }
+        tableView.reloadData()
     }
-    */
 
 }
