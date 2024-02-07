ebd58da01ceac38933b21466b7da415baa29cc45
razmaa <145837511+razmaa@users.noreply.github.com>
2024-02-07 04:55:00 +0400

    Create crypto model

--- CurrnecyConverterApp/Models/CryptoModel.swift
+++ CurrnecyConverterApp/Models/CryptoModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct Crypto: Codable {
+    let id: String
+    let rank: String
+    let symbol: String
+    let name: String
+    let supply: String
+    let maxSupply: String?
+    let marketCapUsd: String
+    let volumeUsd24Hr: String
+    let priceUsd: String
+    let changePercent24Hr: String
+    let vwap24Hr: String?
+}
+
+struct CryptoList: Codable {
+    let data: [Crypto]
+    let timestamp: Int64
+}
